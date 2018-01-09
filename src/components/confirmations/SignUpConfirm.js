d1bcec267700db6e499f7af53cf9118ba58e7978
Trevor Kirpaul <tkirpaul@gmail.com>
2018-01-09 12:06:04 -0500

    styled signup confirm and unverified email components

--- src/components/confirmations/SignUpConfirm.js
+++ src/components/confirmations/SignUpConfirm.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  const: 25px;
+  margin: 10px;
+  padding: 25px;
+  text-align: center;
+  border: 1px solid #383838;
+  background: #e8e8e8;
+  color: #383838;
+`;
+const TextWrapper = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+  margin-bottom: 25px;
+  line-height: 1.8em;
+`;
+const Title = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  font-size: 3em;
+  font-weight: 300;
+`;
+const SubTitle = styled.h3`
+  font-family: 'Roboto', sans-serif;
+`;
+const BodyText = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1em;
+`;
 
 export default () => {
   return (
-    <div>
-      <p>Please check your email for a link to verify your account</p>
-    </div>
+    <Wrapper>
+      <Title>Account Created!</Title>
+      <SubTitle>Please Check Your Email</SubTitle>
+      <TextWrapper>
+        <BodyText>
+          Thank you for creating an account! This web site is a portfolio
+          project that I created to showcase my skills with React, Redux,
+          Express, MongoDB and other modern web development tools.
+        </BodyText>
+        <BodyText>You can view the source code for this project here.</BodyText>
+        <BodyText>
+          To begin using this site, check your email for a verification link.
+          After using the link your account will be verified.
+        </BodyText>
+      </TextWrapper>
+      <SubTitle>
+        Please note that this is a portfolio project and you are not actually
+        buying anything.
+      </SubTitle>
+    </Wrapper>
   );
 };
--- src/components/confirmations/UnverifiedEmail.js
+++ src/components/confirmations/UnverifiedEmail.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  const: 25px;
+  margin: 10px;
+  padding: 25px;
+  text-align: center;
+  border: 1px solid #383838;
+  background: #e8e8e8;
+  color: #383838;
+`;
+const TextWrapper = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+  margin-bottom: 25px;
+  line-height: 1.8em;
+`;
+const Title = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  font-size: 3em;
+  font-weight: 300;
+`;
+const SubTitle = styled.h3`
+  font-family: 'Roboto', sans-serif;
+`;
+const BodyText = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1em;
+`;
 export default () => {
   return (
-    <div>
-      <p>You must verify your email to use this site..</p>
-    </div>
+    <Wrapper>
+      <Title>Please Verify Your Account</Title>
+      <TextWrapper>
+        <BodyText>
+          You are seeing this message because you haven't verified your
+          account's email address.
+        </BodyText>
+        <BodyText>
+          Please check your email for a link to activiate your account.
+        </BodyText>
+        <SubTitle>This site is a portfolio project</SubTitle>
+        <BodyText>
+          Most likely you are just testing this site out and don't want to use a
+          real email address. There is a dummy account available for the purpose
+          of quickly testing the site.
+        </BodyText>
+        <SubTitle>Dummy Account Credentials</SubTitle>
+        <BodyText>email: 'admin'</BodyText>
+        <BodyText>password: 'pass'</BodyText>
+      </TextWrapper>
+    </Wrapper>
   );
 };
